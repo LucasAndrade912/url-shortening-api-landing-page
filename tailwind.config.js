d8db2eb7ee8e1207f42e1d018cb10c363a13383e
Lucas Andrade <lucasantos.dev@gmail.com>
2023-03-31 00:00:54 -0300

    configure global css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		screens: {
+			desktop: '1440px',
+			mobile: '375px',
+		},
+		colors: {
+			primary: {
+				cyan: 'hsl(180, 66%, 49%)',
+				'dark-violet': 'hsl(257, 27%, 26%)',
+			},
+			secondary: {
+				red: 'hsl(0, 87%, 67%)',
+			},
+			neutral: {
+				gray: 'hsl(0, 0%, 75%)',
+				'grayish-violet': 'hsl(257, 7%, 63%)',
+				'very-dark-blue': 'hsl(255, 11%, 22%)',
+				'very-dark-violet': 'hsl(260, 8%, 14%)',
+			},
+		},
 	},
 	plugins: [],
 };
