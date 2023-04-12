edc631d34d5684d822273f219e4f40ab01d9e5a7
Lucas Andrade <lucasantos.dev@gmail.com>
2023-04-11 23:54:16 -0300

    update Advaced Statitcs texts

--- src/components/AdvancedStatistics.tsx
+++ src/components/AdvancedStatistics.tsx
@@ -1,10 +1,12 @@
-import image from '../assets/icon-brand-recognition.svg';
+import brandRecognitionImg from '../assets/icon-brand-recognition.svg';
+import detailedRecordsImg from '../assets/icon-detailed-records.svg';
+import fullyCustomizableImg from '../assets/icon-fully-customizable.svg';
 
 import { StatiticCard } from './StatistcCard';
 
 export function AdvancedStatistics() {
 	return (
-		<section className="bg-neutral-light-gray px-40 pt-28">
+		<section className="bg-neutral-light-gray px-40 pt-28 pb-52">
 			<h2 className="text-4xl font-bold text-neutral-very-dark-blue text-center mb-4">
 				Advanced Statistics
 			</h2>
@@ -18,22 +20,22 @@ export function AdvancedStatistics() {
 				<div className="absolute h-2 bg-primary-cyan w-full inset-y-1/2" />
 
 				<StatiticCard
-					image={{ src: image, alt: 'bla' }}
+					image={{ src: brandRecognitionImg, alt: 'Brand Recognition' }}
 					title="Brand Recognition"
 					text="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content."
 				/>
 
 				<StatiticCard
-					image={{ src: image, alt: 'bla' }}
-					title="Brand Recognition"
-					text="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content."
+					image={{ src: detailedRecordsImg, alt: 'Detailed Records' }}
+					title="Detailed Records"
+					text="Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions."
 					translateY={40}
 				/>
 
 				<StatiticCard
-					image={{ src: image, alt: 'bla' }}
-					title="Brand Recognition"
-					text="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content."
+					image={{ src: fullyCustomizableImg, alt: 'Fully Customizable' }}
+					title="Fully Customizable"
+					text="Improve brand awareness and content discoverability through customizable links, supercharging audience engagament."
 					translateY={80}
 				/>
 			</div>
