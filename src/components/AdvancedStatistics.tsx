fdf7db8eb82e363b72bf3a0ff7aeee9cc059bd12
Lucas Andrade <lucasantos.dev@gmail.com>
2023-04-17 20:04:36 -0300

    add responsive design in Adavanced Statistics section

--- src/components/AdvancedStatistics.tsx
+++ src/components/AdvancedStatistics.tsx
@@ -6,18 +6,17 @@ import { StatiticCard } from './StatistcCard';
 
 export function AdvancedStatistics() {
 	return (
-		<section className="bg-neutral-light-gray px-40 pt-28 pb-52">
-			<h2 className="text-4xl font-bold text-neutral-very-dark-blue text-center mb-4">
+		<section className="bg-neutral-light-gray px-40 pt-28 pb-52 mobile:px-6 mobile:pb-20">
+			<h2 className="text-4xl font-bold text-neutral-very-dark-blue text-center mb-4 mobile:text-[28px]">
 				Advanced Statistics
 			</h2>
 
-			<p className="text-center text-neutral-grayish-violet">
-				Track how your links are performing across the web with <br />
-				our advanced statistcs dashboard.
+			<p className="text-center text-neutral-grayish-violet mobile:text-base">
+				Track how your links are performing across the web with our advanced statistcs dashboard.
 			</p>
 
-			<div className="flex gap-8 mt-32 relative">
-				<div className="absolute h-2 bg-primary-cyan w-full inset-y-1/2" />
+			<div className="flex gap-8 mt-32 relative mobile:flex-col mobile:gap-20 mobile:mt-24">
+				<div className="absolute h-2 bg-primary-cyan w-full inset-y-1/2 mobile:h-full mobile:w-2 mobile:inset-x-1/2 mobile:inset-y-0" />
 
 				<StatiticCard
 					image={{ src: brandRecognitionImg, alt: 'Brand Recognition' }}
--- src/components/StatistcCard.tsx
+++ src/components/StatistcCard.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 interface StatiticCardProps {
 	image: {
 		src: string;
@@ -9,16 +11,32 @@ interface StatiticCardProps {
 }
 
 export function StatiticCard({ image, title, text, translateY = 0 }: StatiticCardProps) {
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+	useEffect(() => {
+		const handleWindowWidth = () => {
+			setWindowWidth(window.innerWidth);
+		};
+
+		window.addEventListener('resize', handleWindowWidth);
+
+		return () => {
+			window.removeEventListener('resize', handleWindowWidth);
+		};
+	}, []);
+
 	return (
 		<div
-			className="bg-white px-8 pb-10 rounded-md z-10 relative"
-			style={{ translate: `0 ${translateY}px` }}
+			className="bg-white px-8 pb-10 rounded-md z-10 relative mobile:text-center"
+			style={{ translate: windowWidth > 375 ? `0 ${translateY}px` : 0 }}
 		>
-			<div className="flex items-center justify-center bg-primary-dark-violet w-20 h-20 rounded-full -translate-y-1/2">
+			<div className="flex items-center justify-center bg-primary-dark-violet w-20 h-20 rounded-full -translate-y-1/2 mobile:-translate-x-1/2 mobile:relative mobile:inset-x-1/2">
 				<img src={image.src} alt={image.alt} />
 			</div>
 
-			<h3 className="text-xl font-bold text-neutral-very-dark-blue mb-3">{title}</h3>
+			<h3 className="text-xl font-bold text-neutral-very-dark-blue mb-3 mobile:text-2xl">
+				{title}
+			</h3>
 
 			<p className="text-neutral-grayish-violet text-sm">{text}</p>
 		</div>
